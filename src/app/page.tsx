0f1014810933c48e6eee7aa52acef5a61b116002
Khanh Pham <qkp.khanh@gmail.com>
2025-03-21 22:39:05 +0700

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,13 @@
 import Image, { type ImageProps } from 'next/image'
 import Link from 'next/link'
-import clsx from 'clsx'
 
 import { Button } from '@/components/Button'
 import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
-import {
-  GitHubIcon,
-  InstagramIcon,
-  LinkedInIcon,
-  XIcon,
-  ZaloIcon,
-} from '@/components/SocialIcons'
+import { GitHubIcon, LinkedInIcon, ZaloIcon } from '@/components/SocialIcons'
 import logoAirbnb from '@/images/logos/airbnb.svg'
 import logoFacebook from '@/images/logos/facebook.svg'
 import logoPlanetaria from '@/images/logos/planetaria.svg'
-import logoStarbucks from '@/images/logos/starbucks.svg'
-import image1 from '@/images/photos/image-1.jpg'
-import image2 from '@/images/photos/image-2.jpg'
-import image3 from '@/images/photos/image-3.jpg'
-import image4 from '@/images/photos/image-4.jpg'
-import image5 from '@/images/photos/image-5.jpg'
 import { type ArticleWithSlug, getAllArticles } from '@/lib/articles'
 import { formatDate } from '@/lib/formatDate'
 
@@ -229,39 +216,6 @@ function Resume() {
   )
 }
 
-function Photos() {
-  const rotations = [
-    'rotate-2',
-    '-rotate-2',
-    'rotate-2',
-    'rotate-2',
-    '-rotate-2',
-  ]
-
-  return (
-    <div className="mt-16 sm:mt-20">
-      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
-          <div
-            key={image.src}
-            className={clsx(
-              'relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
-              rotations[imageIndex % rotations.length],
-            )}
-          >
-            <Image
-              src={image}
-              alt=""
-              sizes="(min-width: 640px) 18rem, 11rem"
-              className="absolute inset-0 h-full w-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-}
-
 export default async function Home() {
   const articles = (await getAllArticles()).slice(0, 4)
 
@@ -270,12 +224,12 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="flex w-full flex-col gap-6">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Hi, I'm Khanh. Welcome to my journey
+            Hi, I&apos;m Khanh. Welcome to my journey
           </h1>
           <p className="mt-4 text-base text-zinc-600 dark:text-zinc-300">
-            I'm a web developer and a curious learner who loves exploring new
-            ideas and sharing knowledge. This blog is where I document what I
-            read, study, and discover—whether it's about problem-solving,
+            I&apos;m a web developer and a curious learner who loves exploring
+            new ideas and sharing knowledge. This blog is where I document what
+            I read, study, and discover—whether it&apos;s about problem-solving,
             creativity, or ways to improve how we work and think.
             <br />
           </p>
