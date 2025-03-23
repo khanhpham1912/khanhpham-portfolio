ba1362e136dbfc76317e1b27506c798f91c75538
Khanh Pham <qkp.khanh@gmail.com>
2025-03-23 22:02:49 +0700

    enhance

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -1,44 +1,9 @@
-import { Card } from '@/components/ui/Card'
-import { Section } from '@/components/ui/Section'
-
-function ToolsSection({
-  children,
-  ...props
-}: React.ComponentPropsWithoutRef<typeof Section>) {
-  return (
-    <Section {...props}>
-      <ul role="list" className="space-y-16">
-        {children}
-      </ul>
-    </Section>
-  )
-}
-
-function Tool({
-  title,
-  href,
-  children,
-}: {
-  title: string
-  href?: string
-  children: React.ReactNode
-}) {
-  return (
-    <Card as="li">
-      <Card.Title as="h3" href={href}>
-        {title}
-      </Card.Title>
-      <Card.Description>{children}</Card.Description>
-    </Card>
-  )
-}
-
 export const metadata = {
-  title: 'Uses',
-  description: 'Software I use, gadgets I love, and other things I recommend.',
+  title: 'Tools',
+  description: 'Some toys',
 }
 
-export default function Uses() {
+export default function Tools() {
   return (
     <div className="mx-auto h-full max-w-7xl">
       <div className="mx-auto h-full max-w-5xl">
