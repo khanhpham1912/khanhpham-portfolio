fbe584a9b2f4fbe2411bcdecb6286fd54e14da25
Khanh Pham <medigo.quangkhanh.pham@gmail.com>
2025-04-21 00:19:22 +0700

    .

---
+++ src/app/t/page.tsx
@@ -0,0 +1,6 @@
+import { useExitIntent } from './useExitIntent'
+
+export default function Page() {
+  const { showExitIntent } = useExitIntent()
+  return <div>showExitIntent:{showExitIntent}</div>
+}
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,13 +1,17 @@
 'use client'
 import { useExitDetector } from './useExitDetector'
+import { useIsMobile } from './useIsMobile'
 
 export default function Test() {
   const { shouldShowModal, showModal, setShouldShowModal, markSurveyAsFilled } =
     useExitDetector({ isExitSignupPopup: false })
 
+  const isMobile = useIsMobile()
+
   return (
     <div>
       <div>{shouldShowModal ? 'Open' : 'Close'}</div>
+      <div>Is Mobile: {isMobile}</div>
     </div>
   )
 }
